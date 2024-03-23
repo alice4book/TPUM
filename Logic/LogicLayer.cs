625318e33c2c7388ee655a7087c6259700b9d718
Klaudia <alice4book@gmail.com>
2024-03-23 17:56:01 +0100

    Rozbudowa warstwy logiki
    Dodanie klasy i interfejsu LogicLayer

---
+++ Logic/LogicLayer.cs
@@ -0,0 +1,21 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class LogicLayer : ILogicLayer
+    {
+        public IShop Shop { get; private set;}
+        private IDataLayer Data { get; }
+
+        public LogicLayer(IDataLayer data)
+        {
+            Data = data;
+            Shop = new Shop(Data.Storage);
+        }
+    }
+}
